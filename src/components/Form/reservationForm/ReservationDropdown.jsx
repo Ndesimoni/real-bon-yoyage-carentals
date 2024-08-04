fa1581042eab1cc3f096371a5db294459dec1df9
ngwasidwan <sidwanche@gmail.com>
2024-08-04 09:28:35 +0100

    handled vehicle reservation form

--- src/components/Form/reservationForm/ReservationDropdown.jsx
+++ src/components/Form/reservationForm/ReservationDropdown.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import { carMakeList } from "../../../DB/Local_Data_Base";
 import styled from "styled-components";
-import { useFormData } from "../../../lib/UrlCustomHook";
-import { useNavigate } from "react-router-dom";
-// import { useForm } from "react-hook-form";
 
-// import { format } from "date-fns";
+import { useForm } from "react-hook-form";
+import FormErrors from "../../FormErrors";
+
+import { useNavigate } from "react-router-dom";
+
+const carType = ["all-cars", "suvs", "luxury-cars", "vans", "trucks"];
 
 export const InputStyles = styled.input({
   border: "1px solid #d2d2d2",
@@ -45,141 +45,105 @@ const Label = styled.label({
 });
 
 function ReservationDropdown() {
-  const [isChecked, setChecked] = useState(false);
-  const { formData, setFormData } = useFormData();
   const navigate = useNavigate();
+  //destructuring the errors property from the forState object provided by react hook forms
 
-  //////changes ////////
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+  function handleForm(data) {
+    //we destructure the category from the data that we receive from our form
+    const { category } = data;
 
-  const handleCheckBox = (e) => {
-    const { name, checked } = e.target;
-    setChecked({ ...formData, [name]: checked });
-  };
+    //we then navigate to the selected vehicle category
+    navigate(`our-vehicle-collection/${category}`);
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   formValidation();
+    console.log(data);
 
-  //   navigate(`category?to=${formData.category}`, {
-  //     state: formData,
-  //   });
-  //   console.log(formData.category);
-  // };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    formValidation();
-
-    //changed the route path to params.
-    navigate(`/All-vehicle-category/${formData.category}`, {
-      state: formData,
-    });
-    console.log(formData.category);
-  };
-
-  function formValidation() {}
+    //we will then use this data where it is needed in the future
+  }
 
   return (
     <div className="flex text-lg mx-1 absolute justify-center ">
-      {/* //todo this is the form starting */}
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(handleForm)}
         className=" gap-10 px-4 py-6 text-lg bg-slate-50 w-[912px] border rounded-b-lg"
         id="form"
       >
-        {/* //todo this is the names */}
         <SectionStyle className=" flex justify-between ">
-          {/* this is for first name */}
           <ItemStyle>
-            <Label>First Name: </Label>
+            <Label htmlFor="firstName">First Name: </Label>
             <InputStyles
+              id="firstName"
               type="text"
               placeholder="First Name"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
+              {...register("firstName", { required: "required field" })}
             />
-            {/* <p className="text-red-500">{error}</p> */}
+            {errors?.firstName && (
+              <FormErrors>{errors.firstName.message}</FormErrors>
+            )}
           </ItemStyle>
 
-          {/* this is for last name */}
           <ItemStyle>
-            <Label>Last Name: </Label>
+            <Label htmlFor="lastName">Last Name: </Label>
             <InputStyles
+              id="lastName"
               type="text"
               placeholder="Last Name"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
+              {...register("lastName", { required: "required field" })}
             />
+            {errors?.lastName && (
+              <FormErrors>{errors.lastName.message}</FormErrors>
+            )}
           </ItemStyle>
         </SectionStyle>
 
-        {/* //todo this is the email */}
         <SectionStyle>
           <ItemStyle>
-            <Label>Email: </Label>
+            <Label htmlFor="email">Email: </Label>
             <InputStylesEmail
               id="email"
               type="email"
               placeholder="boyz@email.com"
-              className="w-full"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
+              {...register("email")}
             />
+            {errors?.email && <FormErrors>{errors.email.message}</FormErrors>}
           </ItemStyle>
         </SectionStyle>
 
-        {/* //todo this is the phone number and Type of car  */}
         <SectionStyle className="flex justify-between mt-3">
-          {/* this is for phone number */}
           <ItemStyle>
             <Label>Phone:</Label>
             <InputStyles
               type="number"
               placeholder="000 000 000"
-              name="phone"
-              value={formData.phone}
-              onChange={handleChange}
+              {...register("phone")}
             />
+            {errors?.phone && <FormErrors>{errors.phone.message}</FormErrors>}
           </ItemStyle>
-          {/* this is for Type of car */}
 
           <ItemStyle>
             <Label>Vehicle Category</Label>
-            <Select
-              name="category"
-              value={formData.category}
-              onChange={handleChange}
-            >
-              {carMakeList.map((items, index) => (
-                <option key={index}>{items.category}</option>
+            <Select {...register("category")}>
+              {carType.map((type, index) => (
+                <option key={index}>{type}</option>
               ))}
             </Select>
           </ItemStyle>
         </SectionStyle>
 
-        {/* //todo this is the location  and dates  */}
         <SectionStyle
           className="flex  flex-row justify-between mt-3"
           id="select-box"
         >
           <div>
-            {/* this is for pick up location */}
             <ItemStyle>
               <Label>Pick-up Location</Label>
 
-              <Select
-                name="pickUpLocation"
-                value={formData.pickUpLocation}
-                onChange={handleChange}
-              >
+              <Select {...register("pickupLocation")}>
                 <option>9500 Good Luck Road MD 20707</option>
                 <option>
                   7900 International Drive Suit 300 Bloomington MN 55425
@@ -193,27 +157,22 @@ function ReservationDropdown() {
               </Select>
             </ItemStyle>
 
-            {/* this is for pick up date */}
             <ItemStyle>
               <Label>Pick-up Date</Label>
               <InputStyles
                 type="date"
-                // name="pickUpDate"
-                // value={formData.pickUpDate}
-                // onChange={handleChange}
+                {...register("pickupDate", { required: "required field" })}
               />
+              {errors?.pickupDate && (
+                <FormErrors>{errors.pickupDate.message}</FormErrors>
+              )}
             </ItemStyle>
           </div>
 
-          {/* this is for drop off */}
           <div>
             <ItemStyle>
               <Label>Drop Off Location</Label>
-              <Select
-              // name="dropOffLocation"
-              // value={formData.dropOffLocation}
-              // onChange={handleChange}
-              >
+              <Select {...register("dropOffLocation")}>
                 <option>9500 Good Luck Road MD 20707</option>
                 <option>
                   7900 International Drive Suit 300 Bloomington MN 55425
@@ -227,15 +186,15 @@ function ReservationDropdown() {
               </Select>
             </ItemStyle>
 
-            {/* this is for drop off date */}
             <ItemStyle>
               <Label className="mb-2">Drop Off Date</Label>
               <InputStyles
                 type="date"
-                //   // name="dropOffDate"
-                //   // value={formData.dropOffDate}
-                //   // onChange={handleChange}
+                {...register("dropOffDate", { required: "required field" })}
               />
+              {errors?.dropOffDate && (
+                <FormErrors>{errors.dropOffDate.message}</FormErrors>
+              )}
             </ItemStyle>
           </div>
         </SectionStyle>
@@ -243,7 +202,14 @@ function ReservationDropdown() {
         <div className="mb-3">
           <ItemStyle className="p-2">
             <p className="text-base p-1">Id Card:</p>
-            <input type="file" className=" text-sm " />
+            <input
+              type="file"
+              className=" text-sm "
+              {...register("nationalID", { required: "required field" })}
+            />
+            {errors?.nationalID && (
+              <FormErrors>{errors.nationalID.message}</FormErrors>
+            )}
           </ItemStyle>
         </div>
 
@@ -254,34 +220,43 @@ function ReservationDropdown() {
               type="number"
               className=" text-sm border max-w-32 px-3 py-1 rounded-sm"
               placeholder=" 18 and Above"
+              {...register("age", {
+                min: {
+                  value: 18,
+                  message: "You must be at least 18 years old",
+                },
+              })}
             />
+            {errors?.age && <FormErrors>{errors.age.message}</FormErrors>}
           </ItemStyle>
         </div>
 
-        <div className=" ml-3 flex align-center gap-2">
-          <div>
+        <div className=" ml-3  gap-2">
+          <div className="flex items-center gap-2">
             <input
               type="checkbox"
               className="h-4 w-4 hover:bg-slate-500 "
-              // value={formData.termConditions}
-              checked={isChecked}
-              name="termConditions"
-              onChange={handleCheckBox}
+              {...register("termConditions", {
+                required: {
+                  value: true,
+                  message:
+                    "please check box above to agree to our terms and conditions",
+                },
+              })}
             />
+            <span className="text-sm">
+              By clicking this button, you confirm our privacy terms and
+              conditions
+            </span>
           </div>
-
-          <Label>
-            By clicking this button, you confirm our privacy terms and
-            conditions
-          </Label>
+          {errors?.termConditions && (
+            <FormErrors>{errors.termConditions.message}</FormErrors>
+          )}
         </div>
 
         <SectionStyle>
           <ItemStyle>
-            <button
-              type="submit"
-              className=" bg-red-600 text-white px-3 py-[1px] uppercase hover:bg-stone-900  hover:text-white transition-all mr-auto rounded-md mt-3"
-            >
+            <button className=" bg-red-600 text-white px-3 py-[1px] uppercase hover:bg-stone-900  hover:text-white transition-all mr-auto rounded-md mt-3">
               Search
             </button>
           </ItemStyle>
@@ -292,10 +267,3 @@ function ReservationDropdown() {
 }
 
 export default ReservationDropdown;
-
-// console.log(firstName);
-
-// if (isChecked === false) return;
-// console.log(formData);
-// let checkStatus = formValidation();
-// console.log(checkStatus);
